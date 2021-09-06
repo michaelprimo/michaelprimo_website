30b3cfc9fa01ec29e7228220c68eb1d29b6d969a
michaelprimo <magiclegendstaff@gmail.com>
2021-09-06 11:44:50 +0200

    Added ww3 test

---
+++ ww3test/script.js
@@ -0,0 +1,714 @@
+(function () {
+    let requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+  })();
+
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+
+let audioCtx = new AudioContext();
+let oscillator = audioCtx.createOscillator();
+
+let notes = new Image();
+let player = new Image();
+let enemy = new Image();
+
+notes.src = "Image/notes.png";
+player.src = "Image/player.png";
+enemy.src = "Image/enemy.png";
+
+ctx.strokeStyle = "white";
+ 
+
+requestAnimationFrame(upload);
+
+
+let dialogueBox = ["Oh, are you approaching me?","Instead of running away, you are coming right through me?"];
+let playerTrees = [];
+let spherePosition = [];
+let leftChart = [];
+let rightChart = [];
+let curNotes = [];
+let levelNotes = [
+[60,0,0,0,0]
+];
+
+let curLevelNotes = [];
+let centralSphere = 
+{
+  x: canvas.width/2-50,
+  y: canvas.height/2-50,
+  width: 100,
+  height: 100,
+}
+
+let game =
+{
+  points: 0,
+  enemyPoints: 0,
+  noteCounter: -canvas.width/2,
+  curNote: 0,
+  maxNotes: 0,
+  positiveNotes: 0,
+  perfect: canvas.width/2-25,
+  noteDistance: canvas.width/4,
+  randomDistance: 4,
+  bpm: 50,
+  defaultBpm: 80,
+  chain: 0,
+  chargeBar: 0,
+  chargeLevel: 1,
+  playerHealth: 100,
+  enemyHealth: 500,
+  playerAttack: 5,
+  enemyAttack: 15
+}
+
+let playerAnimation =
+{
+  frameCount: 0,
+  animation: 0,
+  attackBegin: false,
+  attackEnd: true,
+  hurtBegin: false,
+  hurtEnd: true
+}
+
+let enemyAnimation =
+{
+  frameCount: 0,
+  animation: 0
+}
+
+function loadSpheres()
+{
+  for(let i = 0; i < curLevelNotes.length; i++)
+  {
+    let leftSphere = {};
+    let rightSphere;
+    leftSphere.x = game.noteCounter;
+    leftSphere.y = canvas.height/2;
+    leftSphere.type = curLevelNotes[i];
+    leftSphere.speed = 8/game.randomDistance;
+    
+    if(leftSphere.type <= 4)
+    {
+      leftSphere.radius = 32;
+      leftSphere.width = 32;
+      leftSphere.height = 32;
+      leftChart.push(leftSphere);
+      rightSphere = {...leftSphere};
+      rightChart.push(rightSphere);
+      rightChart[i].x = canvas.width-rightChart[i].width*2 + Math.abs(game.noteCounter);
+      game.noteCounter -= game.noteDistance;
+    }
+    
+  }
+  /*
+  game.noteCounter = canvas.width;
+  for(let i = 0; i < rightChart.length; i++)
+  {
+    
+    rightChart[i].x += game.noteCounter;
+    game.noteCounter += game.bpm*2;
+  }
+  */
+}
+
+
+function setSpheres()
+{
+  for(let i = 1; i < levelNotes[0].length; i++)
+  {
+    for(let j = 0; j < levelNotes[0][i]; j++)
+    {
+      curLevelNotes.push(i);
+    }
+  }
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function setBeatSpheres()
+{
+  for(let i = 0; i < levelNotes[0][0]; i++)
+  {
+    curLevelNotes.splice(i*4,0,0);
+  }
+}
+
+function setMaxNotes()
+{
+  for(let i = 0; i < levelNotes[0].length; i++)
+  {
+    game.maxNotes += levelNotes[0][i];
+  }
+  game.positiveNotes += levelNotes[0][0] + levelNotes[0][1]; 
+}
+
+setSpheres();
+shuffle(curLevelNotes);
+setBeatSpheres();
+loadSpheres();
+loadTrees();
+setMaxNotes();
+
+
+function drawPlayer()
+{
+  playerAnimation.frameCount++;
+  //if(playerAnimation.hurtBegin == false && playerAnimation.attackBegin == false)
+  //{
+    idleAnimation();
+  //}
+  /*
+  if(playerAnimation.attackBegin == true)
+  {
+    attackAnimation();
+    playerAnimation.attackBegin = true;
+  }
+  if(playerAnimation.hurtBegin == true)
+  {
+    hurtAnimation();
+    
+    
+  }
+  */
+  if(playerAnimation.frameCount > 30)
+  {
+    playerAnimation.frameCount = 0;
+  }
+  ctx.drawImage(player, 64*playerAnimation.animation, 0, 64, 64, canvas.width/2-32, canvas.height/4*3-32, 64, 64);
+}
+
+function idleAnimation()
+{
+  if(playerAnimation.frameCount > 15)
+  {
+    playerAnimation.animation = 1;
+  }
+  if(playerAnimation.frameCount <= 15 || playerAnimation.frameCount > 30)
+  {
+    playerAnimation.animation = 0;
+  }
+  
+}
+
+function attackAnimation()
+{
+  playerAnimation.frameCount++;
+  playerAnimation.animation = 2;
+  playerAnimation.frameCount = 0;
+  if(playerAnimation.frameCount > 15)
+  {
+    playerAnimation.attackBegin = false;
+    idleAnimation();
+  }
+  enemyAnimation.animation = 3;
+  enemyAnimation.frameCount = 0;
+}
+
+function hurtAnimation()
+{
+  playerAnimation.frameCount++;
+  playerAnimation.animation = 3;
+  playerAnimation.frameCount = 0;
+  if(playerAnimation.frameCount > 15)
+  {
+    playerAnimation.hurtBegin = false;
+    idleAnimation();
+  }
+  enemyAnimation.animation = 2;
+  enemyAnimation.frameCount = 0;
+}
+
+function chargeAnimation()
+{
+  playerAnimation.animation = 4;
+  playerAnimation.frameCount = 0;
+}
+
+function drawEnemy()
+{
+  enemyAnimation.frameCount++;
+  if(enemyAnimation.frameCount > 15)
+  {
+    enemyAnimation.animation = 1;
+  }
+  if(enemyAnimation.frameCount <= 15 || enemyAnimation.frameCount > 30)
+  {
+    enemyAnimation.animation = 0;
+  }
+  if(enemyAnimation.frameCount > 30)
+  {
+    enemyAnimation.frameCount = 0;
+  }
+  ctx.drawImage(enemy, 64*enemyAnimation.animation, 0, 64, 64, canvas.width/2-32, canvas.height/4-32, 64, 64);
+}
+
+function upload()
+{
+  
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle = "#0D1B2A";
+  ctx.fillRect(0,0,canvas.width,canvas.height/2);
+  ctx.fillStyle = "#0D1B2A";
+  ctx.fillRect(0,canvas.height/2,canvas.width,canvas.height);
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "white";
+  
+  
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/2, canvas.height/3.45, 45, 0, 2 * Math.PI);
+  
+  ctx.moveTo(0, canvas.height/2);
+  ctx.lineTo(canvas.width, canvas.height/2);
+  
+  ctx.closePath();
+  ctx.stroke();
+  
+  ctx.beginPath();
+  ctx.arc(canvas.width/2, canvas.height/4*2.84, 45, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  ctx.strokeRect(centralSphere.x, centralSphere.y, centralSphere.width, centralSphere.height);
+  ctx.beginPath();
+  
+  if(leftChart.length > 0)
+  {
+    
+      if((leftChart[0].x > game.perfect))
+        {
+          missNote();
+        }
+    
+      for(let i = 0; i < leftChart.length; i++)
+      {
+        
+        if(leftChart[i].type <= 4)
+        {
+          
+            ctx.drawImage(notes,
+              leftChart[i].width*leftChart[i].type, 0, leftChart[i].width, leftChart[i].height,
+              leftChart[i].x, leftChart[i].y-leftChart[i].height, leftChart[i].width*2, leftChart[i].height*2);
+          
+          
+          ctx.drawImage(notes,
+            rightChart[i].width*rightChart[i].type, 0, rightChart[i].width, rightChart[i].height,
+              rightChart[i].x, rightChart[i].y-rightChart[i].height, rightChart[i].width*2, rightChart[i].height*2); 
+           
+        }
+        
+      }
+    }
+    
+  ctx.closePath();
+  ctx.stroke();
+  drawLayout();
+  ctx.fillStyle = "white";
+  ctx.font = "50px Arial";
+  /*
+  ctx.fillText(game.playerHealth, canvas.width/3,canvas.height/10);
+  ctx.fillText(game.enemyHealth, canvas.width/3*2,canvas.height/10);
+  ctx.fillText(game.chain, canvas.width/4,canvas.height/10*9);
+  ctx.fillText(game.chargeLevel, canvas.width/2,canvas.height/10*9);
+  ctx.fillText(game.chargeBar, canvas.width/3*2,canvas.height/10*9);
+  //ctx.fillText(game.enemyPoints, canvas.width/3,canvas.height/10*9.5);
+  ctx.fillText("Chain: " + game.randomDistance, canvas.width/5,canvas.height/2-55);
+  ctx.fillText("BPM: " + game.bpm.toFixed(2), canvas.width/5,canvas.height/9*6-20);
+  */
+  for(let i = 0; i<leftChart.length; i++)
+  {
+    leftChart[i].x += (((canvas.width/2)/60*(game.bpm/60))/leftChart[i].speed);
+    rightChart[i].x -= ((canvas.width/2)/60*(game.bpm/60))/rightChart[i].speed;
+    //leftChart[i].x += 0;
+    //rightChart[i].x -= 0;
+  }
+  requestAnimationFrame(upload);
+}
+
+function drawLayout()
+{
+  //drawPlayer();
+  //drawEnemy();
+  drawButtons();
+  drawTimer();
+  //drawTrees();
+  //drawBullets();
+}
+
+function drawButtons()
+{
+  ctx.strokeStyle = "white";
+  ctx.strokeRect(0,canvas.height/20*17,canvas.width,canvas.height/6.5);
+
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/8, canvas.height-canvas.height/14, canvas.height/20, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/2.6, canvas.height-canvas.height/14, canvas.height/20, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/1.6, canvas.height-canvas.height/14, canvas.height/20, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/1.15, canvas.height-canvas.height/14, canvas.height/20, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+
+}
+
+function drawTimer()
+{
+  ctx.font = "25px Consolas";
+  ctx.strokeStyle = "white";
+  //ctx.strokeRect(0,0,canvas.width,canvas.height/6.5);
+  
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/8, canvas.height/14, canvas.height/20, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fillText("Enemy", 12.5, canvas.height/6);
+  ctx.fillText("Lives", 12.5, canvas.height/6+25);
+
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/2.6, canvas.height/14, canvas.height/20, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.fillText("Enemy", 12.5, canvas.height/6);
+  ctx.fillText("Lives", 12.5, canvas.height/6+25);
+
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/1.6, canvas.height/14, canvas.height/20, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.lineWidth = 3;
+  ctx.arc(canvas.width/1.15, canvas.height/14, canvas.height/20, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.stroke();
+}
+
+function drawBullets()
+{
+  ctx.strokeStyle = "#47B39D";
+  ctx.fillStyle = "#47B39D";
+  ctx.strokeRect(0,canvas.height/6.5,80,canvas.height-canvas.height/3.3);
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width/7.5, canvas.height/4.5, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width/15, canvas.height/3.1, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width/7.5, canvas.height/2.2, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width/15, canvas.height/1.8, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width/7.5, canvas.height/1.48, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width/15, canvas.height/1.3, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.strokeStyle = "#EB6B56";
+  ctx.fillStyle = "#EB6B56";
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width - canvas.width/7.5, canvas.height/4.5, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width - canvas.width/15, canvas.height/3.1, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width - canvas.width/7.5, canvas.height/2.2, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width - canvas.width/15, canvas.height/1.8, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width - canvas.width/7.5, canvas.height/1.48, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.ellipse(canvas.width - canvas.width/15, canvas.height/1.3, 32, 16, Math.PI / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.fill();
+  
+  ctx.strokeRect(canvas.width-80,canvas.height/6.5,canvas.width-50,canvas.height-canvas.height/3.3);
+}
+
+
+function loadTrees()
+{
+  
+  for(let i = 0; i < 100; i++)
+  {
+    let playerTree = 
+    {
+      x: (Math.random() * canvas.width)-16,
+      y: canvas.height-canvas.height/2.55,
+      radiusX: 16,
+      radiusY: 4,
+      rotation: Math.PI / 2,
+      startAngle: 0,
+      endAngle: 2 * Math.PI
+    }
+  let randDirection = Math.floor(Math.random() * 4);
+  switch(randDirection)
+  {
+    case 1:
+      playerTree.x = (Math.random() * canvas.width)-16;
+      playerTree.y = canvas.height-canvas.height/5;
+      break;
+    case 2:
+      playerTree.x = (Math.random() * canvas.width)-16;
+      playerTree.y = canvas.height-canvas.height/2.55;
+      break;
+    case 3:
+      playerTree.x = (Math.random() * canvas.width - canvas.width/3)-16;
+      playerTree.y = canvas.height-canvas.height/3.55;
+      break;
+    case 4:
+      playerTree.x = (Math.random() * canvas.width - canvas.width/3)-16;
+      playerTree.y = canvas.height-canvas.height/3;
+      break;
+  }
+  playerTrees.push(playerTree);
+    
+  }
+  
+ 
+}
+
+function drawTrees()
+{
+  for(let i = 0; i < playerTrees.length; i++)
+  {
+    ctx.beginPath();
+    ctx.ellipse(playerTrees[i].x,playerTrees[i].y,playerTrees[i].radiusX,playerTrees[i].radiusY,playerTrees[i].rotation,playerTrees[i].startAngle,playerTrees[i].endAngle);
+    ctx.stroke();
+    ctx.fill();
+  }
+}
+
+function checkNote(cursorX, cursorY)
+{
+  
+    if(leftChart[0].x > game.perfect-50)
+    {
+        if(leftChart[0].type == 0)
+        {
+          enemyHit();
+          playSound();
+        }
+        if(leftChart[0].type == 1)
+        {  
+          playSound();
+        }
+        if(leftChart[0].type == 2)
+        {
+          playerMiss();
+        }
+        if(leftChart[0].type == 3)
+        {
+          playerMiss();
+        }
+        if(leftChart[0].type == 4)
+        {
+          playSound();
+          //changeDistance();
+        }
+        leftChart.splice(0,1);
+        rightChart.splice(0,1);
+    }
+  
+  
+  
+
+
+}
+
+function missNote()
+{
+  if(leftChart[game.curNote].type == 0)
+  {
+    playerMiss();
+  }
+  if(leftChart[game.curNote].type == 1)
+  { 
+    playerMiss();
+  }
+  if(leftChart[game.curNote].type == 2)
+  {
+    playSound();
+  }
+  if(leftChart[game.curNote].type == 3)
+  {
+    playSound();
+    changeBpm();
+  }
+  if(leftChart[game.curNote].type == 4)
+  { 
+    playerMiss();
+  }
+  
+  leftChart.splice(0,1);
+  rightChart.splice(0,1);
+}
+
+function playerMiss()
+{
+  game.chain = 0;
+  game.playerHealth -= game.enemyAttack;
+}
+
+function enemyHit()
+{
+  game.chain++; 
+  game.enemyHealth -= game.playerAttack;
+}
+
+function changeBpm()
+{
+  game.bpm = (Math.random() * game.defaultBpm/2) + game.defaultBpm;
+}
+
+function changeDistance()
+{
+  if(leftChart.length > 0)
+  {
+    game.noteCounter = 0;
+    game.randomDistance = Math.floor(Math.random() * 5) + 2;
+    for(let i = 1; i < leftChart.length; i++)
+    {
+      //game.noteDistance = (Math.random() * canvas.width/2 - canvas.width/8) + canvas.width/8;
+      game.noteDistance = canvas.width/2;
+      leftChart[i].x = leftChart[0].x - game.noteCounter - game.noteDistance;
+      //leftChart[i].speed = 8/game.randomDistance;
+      rightChart[i].x = rightChart[0].x + game.noteCounter + game.noteDistance;
+      //rightChart[i].speed = 8/game.randomDistance;
+      game.noteCounter += game.noteDistance;
+    }
+  }
+}
+
+function playSound()
+{
+  /*
+  with(new AudioContext)
+      with(G=createGain())
+      for(i in D=[Math.floor(Math.random() * 15 + 5)])
+      with(createOscillator())
+      if(D[i])
+      connect(G),
+      G.connect(destination),
+      start(i*1/((game.bpm*4)/60)),
+      frequency.setValueAtTime(440*1.06**(13-D[i]),i*1/((game.bpm*4)/60)),
+      gain.setValueAtTime(1,i*1/((game.bpm*4)/60)),
+      gain.setTargetAtTime(.0001,i*1/((game.bpm*4)/60)+.23,.005),
+      stop(i*1/((game.bpm*4)/60)+.24);
+      */
+}
+
+document.onkeydown = function(e)
+{
+  if(e.keyCode == 32)
+  {
+    checkNote();
+  }
+}
+
+
+document.onmousedown = function(e)
+{
+  let cursorX = e.clientX;
+  let cursorY = e.clientY;
+  checkNote(cursorX, cursorY);
+}
+
+/*
+canvas.click(function(e){
+  
+});
+*/
+
+/*
+function testAndExecute(elem, event, area, func){
+  // area is relative to page :
+  // var area = {
+  //     x0: 60,
+  //     y0: 60,
+  //     x1: 100,
+  //     y1: 100
+  // }    
+  elem.addEventListener(event, function(e){
+      var page = {
+          x: e.pageX,
+          y: e.pageY
+      }
+      if(page.x >= area.x0 && page.x <= area.x1 && page.y >= area.y0 && page.y <= area.y1){
+          func();
+      }
+  });
+}
+
+testAndExecute(
+  document.getElementById('example'),
+  'click',
+  {
+      x0: 60,
+      x1: 100,
+      y0: 60,
+      y1: 100
+  
+  },
+  function(){
+      alert('It worked!');
+  }
+);
+*/
