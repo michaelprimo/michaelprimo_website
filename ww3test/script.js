46f47ddbc362bda3752ec6ca5d6465bc3ec7867e
michaelprimo <magiclegendstaff@gmail.com>
2021-09-08 12:36:02 +0200

    test

--- ww3test/script.js
+++ ww3test/script.js
@@ -7,9 +7,9 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 
-if(window.innerWidth > 500)
+if(window.innerWidth > 568)
 {
-canvas.width = 500;
+canvas.width = 568;
 }
 else
 {
@@ -42,7 +42,7 @@ let leftChart = [];
 let rightChart = [];
 let curNotes = [];
 let levelNotes = [
-[15,30,15]
+[30,15,15,15,15,15,15]
 ];
 
 let curLevelNotes = [];
@@ -57,28 +57,20 @@ colorData: -1
 
 let game =
 {
-points: 0,
-enemyPoints: 0,
-noteCounter: -canvas.width/2,
-curNote: 0,
-maxNotes: 0,
-positiveNotes: 0,
-perfect: canvas.width/2,
-noteDistance: canvas.width/4,
-randomDistance: 2,
-bpm: 80,
-defaultBpm: 80,
-bool_shutdown: false,
-playerHealth: 40,
-max_playerHealth: 40,
-enemyHealth: 40,
-max_enemyHealth: 40,
-musicBullets: 6,
-max_musicBullets: 6,
-click: 0,
-chain: 0,
-enemyCharge: 0,
-max_enemyCharge: 6
+  level: 0,
+  noteCounter: -canvas.width/2,
+  maxNotes: 0,
+  perfect: canvas.width/2,
+  noteDistance: canvas.width/4,
+  randomDistance: 2,
+  bpm: 200,
+  defaultBpm: 200,
+  playerHealth: 40,
+  max_playerHealth: 40,
+  enemyHealth: 30,
+  max_enemyHealth: 30,
+  curNotes: 0,
+  chain: 0,
 }
 
 let playerAnimation =
@@ -109,18 +101,22 @@ for(let i = 0; i < curLevelNotes.length; i++)
 
   leftSphere.speed = 8/game.randomDistance;
   
-  if(leftSphere.type <= 2)
+  if(leftSphere.type == 3 || leftSphere.type == 4)
+  {
+    leftSphere.radius = canvas.width/50;
+  }
+  else
   {
     leftSphere.radius = canvas.width/20;
-    leftSphere.width = 32;
-    leftSphere.height = 32;
+  }
     leftChart.push(leftSphere);
     rightSphere = {...leftSphere};
     rightChart.push(rightSphere);
     rightChart[i].x = canvas.width + Math.abs(game.noteCounter);
-    game.noteDistance = Math.floor(Math.random()*canvas.width/4) + canvas.width/5;
+    //game.noteDistance = Math.floor(Math.random()*canvas.width/4) + canvas.width/6;
+    game.noteDistance = canvas.width/6;
     game.noteCounter -= game.noteDistance;
-  }
+  
   
 }
 /*
@@ -185,213 +181,159 @@ ctx.arc(centralSphere.x, centralSphere.y, centralSphere.radius, 0, 2 * Math.PI);
 ctx.stroke();
 }
 
-function drawPlayer2()
-{
-ctx.beginPath();
-ctx.arc(canvas.width/2, canvas.height/4*2.84, 45, 0, 2 * Math.PI);
-ctx.stroke();
-playerAnimation.frameCount++;
-//if(playerAnimation.hurtBegin == false && playerAnimation.attackBegin == false)
-//{
-  idleAnimation();
-//}
-/*
-if(playerAnimation.attackBegin == true)
-{
-  attackAnimation();
-  playerAnimation.attackBegin = true;
-}
-if(playerAnimation.hurtBegin == true)
-{
-  hurtAnimation();
-  
-  
-}
-*/
-if(playerAnimation.frameCount > 30)
-{
-  playerAnimation.frameCount = 0;
-}
-ctx.drawImage(player, 64*playerAnimation.animation, 0, 64, 64, canvas.width/2-32, canvas.height/4*3-32, 64, 64);
-}
-
-function idleAnimation()
-{
-if(playerAnimation.frameCount > 15)
-{
-  playerAnimation.animation = 1;
-}
-if(playerAnimation.frameCount <= 15 || playerAnimation.frameCount > 30)
-{
-  playerAnimation.animation = 0;
-}
-
-}
-
-function attackAnimation()
-{
-playerAnimation.frameCount++;
-playerAnimation.animation = 2;
-playerAnimation.frameCount = 0;
-if(playerAnimation.frameCount > 15)
-{
-  playerAnimation.attackBegin = false;
-  idleAnimation();
-}
-enemyAnimation.animation = 3;
-enemyAnimation.frameCount = 0;
-}
-
-function hurtAnimation()
-{
-playerAnimation.frameCount++;
-playerAnimation.animation = 3;
-playerAnimation.frameCount = 0;
-if(playerAnimation.frameCount > 15)
-{
-  playerAnimation.hurtBegin = false;
-  idleAnimation();
-}
-enemyAnimation.animation = 2;
-enemyAnimation.frameCount = 0;
-}
-
-function chargeAnimation()
-{
-playerAnimation.animation = 4;
-playerAnimation.frameCount = 0;
-}
-
 function drawEnemy()
 {
 ctx.beginPath();
-ctx.arc(canvas.width/2, canvas.height/3.45, 45, 0, 2 * Math.PI);
+ctx.arc(0, canvas.height/2, canvas.width/20, 0, 2 * Math.PI);
+ctx.arc(canvas.width, canvas.height/2, canvas.width/20, 0, 2 * Math.PI);
 ctx.closePath();
 ctx.stroke();
-enemyAnimation.frameCount++;
-if(enemyAnimation.frameCount > 15)
-{
-  enemyAnimation.animation = 1;
 }
-if(enemyAnimation.frameCount <= 15 || enemyAnimation.frameCount > 30)
+
+function drawBackground()
 {
-  enemyAnimation.animation = 0;
+  
+  ctx.fillStyle = "#0D1B2A";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "white";
 }
-if(enemyAnimation.frameCount > 30)
+
+function drawBoard()
 {
-  enemyAnimation.frameCount = 0;
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height/2);
+  ctx.lineTo(canvas.width, canvas.height/2);
+  ctx.closePath();
 }
-ctx.drawImage(enemy, 64*enemyAnimation.animation, 0, 64, 64, canvas.width/2-32, canvas.height/4-32, 64, 64);
+
+function drawText()
+{
+  ctx.fillStyle = "blue";
+  ctx.beginPath();
+          ctx.arc(canvas.width/3, canvas.height/5.8, canvas.height/30, 0, 2 * Math.PI);
+          ctx.stroke();
+          ctx.fill();
+          ctx.closePath();
+  ctx.strokeStyle = "white";
+  ctx.fillStyle = "white";
+  ctx.font = "50px Palatino Linotype";
+  ctx.fillText(Math.floor(game.max_playerHealth-game.playerHealth), canvas.width/5,canvas.height/5);
+  ctx.fillText("x " + (game.curNotes) + " /  " + game.maxNotes, canvas.width/2.5,canvas.height/5);
+  //ctx.fillText(Math.floor(game.bpm), canvas.width/4,canvas.height/4);
+  //ctx.fillText(Math.floor(game.chain), canvas.width-canvas.width/4,canvas.height/4);
+}
+
+function drawNotes()
+{ 
+  if(leftChart.length > 0)
+  {
+    
+      if((leftChart[0].x > game.perfect))
+        {
+          avoidNote();
+        }
+    
+      //draw notes
+      for(let i = 0; i < leftChart.length; i++)
+      {
+        ctx.lineWidth = 3;
+        ctx.strokeStyle = "white";
+        switch(leftChart[i].type)
+        {
+          case 0:
+            ctx.fillStyle = "white";
+            ctx.strokeStyle = "black";
+            break;
+          case 2:
+          case 4:
+            ctx.fillStyle = "blue";
+            break;
+          case 1:
+          case 3:
+            ctx.fillStyle = "red";
+            break;
+          case 5:
+            ctx.lineWidth = 7;
+            ctx.fillStyle = "#0D1B2A";
+            ctx.strokeStyle = "red";
+            break;
+          case 6:
+            ctx.lineWidth = 7;
+            ctx.fillStyle = "#0D1B2A";
+            ctx.strokeStyle = "blue";
+            break;
+        }
+          ctx.beginPath();
+          ctx.arc(leftChart[i].x, leftChart[i].y, leftChart[i].radius, 0, 2 * Math.PI);
+          ctx.stroke();
+          ctx.fill();
+          ctx.closePath();
+  
+          ctx.beginPath();
+          ctx.arc(rightChart[i].x, rightChart[i].y, rightChart[i].radius, 0, 2 * Math.PI);
+          ctx.stroke();
+          ctx.fill();
+          ctx.closePath();
+      }
+    }
+    
+  for(let i = 0; i<leftChart.length; i++)
+  {
+    leftChart[i].x += (((canvas.width/2)/60*(game.bpm/60))/leftChart[i].speed);
+    rightChart[i].x -= ((canvas.width/2)/60*(game.bpm/60))/rightChart[i].speed;
+  }
 }
 
 function upload()
 {
-changeBpm();
-ctx.clearRect(0,0,canvas.width,canvas.height);
-ctx.fillStyle = "#0D1B2A";
-ctx.fillRect(0,0,canvas.width,canvas.height/2);
-ctx.fillStyle = "#0D1B2A";
-ctx.fillRect(0,canvas.height/2,canvas.width,canvas.height);
-ctx.fillStyle = "white";
-ctx.strokeStyle = "white";
-
-ctx.beginPath();
-ctx.lineWidth = 3;
-ctx.moveTo(0, canvas.height/2);
-ctx.lineTo(canvas.width, canvas.height/2);
-ctx.closePath();
-
-
-
-
-
-
-ctx.beginPath();
-
-if(leftChart.length > 0)
-{
-  
-    if((leftChart[0].x > game.perfect))
-      {
-        avoidNote();
-      }
-  
-    //draw notes
-    for(let i = 0; i < leftChart.length; i++)
-    {
-      switch(leftChart[i].type)
-      {
-        case 0:
-          ctx.fillStyle = "blue";
-          break;
-        case 1:
-          ctx.fillStyle = "red";
-          break;
-        case 2:
-          ctx.fillStyle = "blue";
-          break;
-      }
-        ctx.beginPath();
-        ctx.arc(leftChart[i].x, leftChart[i].y, leftChart[i].radius, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill();
-        ctx.closePath();
-
-        ctx.beginPath();
-        ctx.arc(rightChart[i].x, rightChart[i].y, rightChart[i].radius, 0, 2 * Math.PI);
-        ctx.stroke();
-        ctx.fill();
-        ctx.closePath();
-    }
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  switch(game.level)
+  {
+    case 0:
+      drawMenu();
+      break;
+    case 1:
+      changeBpm();
+      drawLayout();
+      break;
   }
   
-ctx.closePath();
-ctx.stroke();
-drawLayout();
-ctx.fillStyle = "white";
-ctx.font = "50px Palatino Linotype";
-ctx.fillText(Math.floor(game.playerHealth), canvas.width/5,canvas.height/5);
-ctx.fillText(Math.floor(game.enemyHealth), canvas.width-canvas.width/5,canvas.height/5);
-ctx.fillText(Math.floor(game.bpm), canvas.width/4,canvas.height/4);
-ctx.fillText(Math.floor(game.chain), canvas.width-canvas.width/4,canvas.height/4);
-/*
-ctx.fillText(game.playerHealth, canvas.width/3,canvas.height/10);
-ctx.fillText(game.enemyHealth, canvas.width/3*2,canvas.height/10);
-ctx.fillText(game.chain, canvas.width/4,canvas.height/10*9);
-ctx.fillText(game.chargeLevel, canvas.width/2,canvas.height/10*9);
-ctx.fillText(game.chargeBar, canvas.width/3*2,canvas.height/10*9);
-//ctx.fillText(game.enemyPoints, canvas.width/3,canvas.height/10*9.5);
-ctx.fillText("Chain: " + game.randomDistance, canvas.width/5,canvas.height/2-55);
-ctx.fillText("BPM: " + game.bpm.toFixed(2), canvas.width/5,canvas.height/9*6-20);
-*/
-for(let i = 0; i<leftChart.length; i++)
-{
-  leftChart[i].x += (((canvas.width/2)/60*(game.bpm/60))/leftChart[i].speed);
-  rightChart[i].x -= ((canvas.width/2)/60*(game.bpm/60))/rightChart[i].speed;
-  //leftChart[i].x += 0;
-  //rightChart[i].x -= 0;
-}
-requestAnimationFrame(upload);
+  requestAnimationFrame(upload);
 }
 
+function drawMenu()
+{
+  ctx.fillStyle = "#0D1B2A";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+  ctx.fillStyle = "white";
+  ctx.strokeStyle = "white";
+  ctx.font = "5em Cortana";
+  ctx.fillText("Wonder", 0, 100);
+  ctx.fillText("Wanderer", 0, 200);
+  ctx.fillText("3", 0, 300);
+}
 function drawLayout()
 {
-drawPlayer();
-
+  drawBackground();
+  drawPlayer();
+  drawEnemy();
+  drawBoard();
+  drawText();
+  drawNotes();
 }
 
 function checkNote(cursorX, cursorY)
 {
-game.click++;
 
 if(leftChart[0].x > game.perfect-canvas.width/7)
 {
-    if(leftChart[0].type == 1 )
+    if(leftChart[0].type == 1 || leftChart[0].type == 3 || leftChart[0].type == 5)
     {
       game.playerHealth--;
       game.chain = 0;
     }
-    if(leftChart[0].type == 2 || leftChart[0].type == 0)
+    if(leftChart[0].type == 2 || leftChart[0].type == 0 || leftChart[0].type == 4 || leftChart[0].type == 6)
     {
       game.enemyHealth--;
       game.chain++;
@@ -407,32 +349,34 @@ else
 
 function avoidNote()
 {
-if(leftChart[0].type == 1 )
-    {
-      game.chain++;
-    }
-    if(leftChart[0].type == 2 || leftChart[0].type == 0)
-    {
-      game.chain = 0;
-    }
-    clearNote();
+  if(leftChart[0].type == 1 || leftChart[0].type == 3 || leftChart[0].type == 5)
+  {
+    game.chain++;
+  }
+  if(leftChart[0].type == 2 || leftChart[0].type == 0 || leftChart[0].type == 4 || leftChart[0].type == 6)
+  {
+    game.playerHealth--;
+    game.chain = 0;
+  }
+  clearNote();
 }
 
 function missNote()
 {
-game.playerHealth--;
-game.chain = 0;
+  game.playerHealth--;
+  game.chain = 0;
 }
 
 function clearNote()
 { 
+  game.curNotes++;
   leftChart.splice(0,1);
   rightChart.splice(0,1);
 }
 
 function changeBpm()
 {
-  game.bpm = game.defaultBpm + (game.defaultBpm/2/game.maxNotes)*game.chain;
+  game.bpm = game.defaultBpm + (game.defaultBpm/2/game.maxNotes)*game.curNotes;
 }
 
 function playSound()
@@ -458,7 +402,13 @@ document.onmousedown = function(e)
 let rect = canvas.getBoundingClientRect();
 let cursorX = e.clientX - rect.left;
 let cursorY = e.clientY - rect.top;
-checkNote(cursorX, cursorY);
+switch(game.level)
+{
+  case 0:
+    game.level++;
+  case 1:
+    checkNote(cursorX, cursorY);
+}
 e.preventDefault();
 }
 /*
